997657eaef737db62998c77a3bbc3c6528faa5d0
Lars Ivar Hatledal <larsivarhatledal@gmail.com>
2023-08-26 18:35:10 +0200

    Update Greet.cpp

--- src/personLib/Greet.cpp
+++ src/personLib/Greet.cpp
@@ -8,5 +8,5 @@ using namespace exercise1;
 
 void exercise1::greet(const Person& person) { // we don't mutate, so const.
 
-    std::cout << person.getFullName() << std::endl;
+    std::cout << "Hello, " << person.getFullName() << "!" << std::endl;
 }
